802578932dca44d9bed9545bd7086c5521a53569
jan <jan@ivotingcentre.ee>
2019-09-10 09:31:04 +0300

    course done to 2-way data bind, not included

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,83 @@
-import { Component } from '@angular/core';
+import { Component} from "@angular/core";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
+
+    <!--    property binding-->
     <div class="app">
-      Hello!
+      {{title + '!'}}
     </div>
+    <h1 [innerHTML]="title"></h1>
+    <div>
+      {{numberOne + numberTwo}}
+
+    </div>
+    <div>
+      {{isHappy ? ':)' : ':('}}
+    </div>
+
+    <img [src]="logo">
+
+    <!--    one way databinding-->
+    <input type="text" [value]="name">
+    <div>
+      {{name}}
+    </div>
+
+
+    <!--    event binding-->
+    <hr>
+    <hr>
+    <br>
+    <br>
+
+    <div class="app">
+
+      <!--      blur - if user inputs something and clicks somewhere else, then name is changing-->
+      <!--      input - if user inputs something, then name is changing every time something is typed. Its actually still one way databind with event being emitted-->
+      <input
+        type="text"
+        [value]="name"
+        (input) = "handleInput($event)"
+        (blur)="handleBlur($event)"
+      >
+    </div>
+    <div>
+      {{name}}
+    </div>
+
+    <!--1 way databind with click event-->
+    <button
+      (click)="handleClick()"
+    >Change name</button>
   `
 })
+
 export class AppComponent {
+  title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = false;
+  logo: string = 'img/angle-down.svg';
+  name: string = "Jan";
+
+  constructor() {
+    this.title = "Ultimate angular"
+  }
+
+  handleBlur(event: any) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+
+  handleClick() {
+    this.name = "Pentshuk";
+  }
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
 
 @NgModule({
-  imports: [
-    BrowserModule
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+    CommonModule,
+    FormsModule
   ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
+
